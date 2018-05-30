111be423103db36eda470b08b4c6b0cbaa7558f6
Zenov007 <36129938+Zenov007@users.noreply.github.com>
2018-05-30 12:41:08 +0300

    Update matrix_form.cpp
    New file with rite matrix

--- matrix_form.cpp
+++ matrix_form.cpp
@@ -1,26 +1,17 @@
-﻿//Симметричную матрицу А(N, N), заданную нижним треугольником в виде одномерного массива 
+//Симметричную матрицу А(N, N), заданную нижним треугольником в виде одномерного массива 
 //относительно побочной диагонали, умножить на вектор В(N).В памяти матрицу не получать.
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include<iomanip>
 using namespace std;
-typedef int fff(int **, int *, int);
+typedef int fff(int *, int *, int);
 typedef fff *pfff;
-void giveMemory(int **&A, const int N) {
-	try { A = new int*[N]; }
+void giveMemory(int *&A, const int N) {
+	try { A = new int[N*(N+1)/2]; }
 	catch (...) {
 		cout << "Ошибка: невозможно создать массив для строк.";
 		exit(0);
 	}
-
-	for (int i = 0; i<N; i++) {
-		try { A[i] = new int[N]; }
-		catch (...) {
-			cout << "Ошибка: невозможно создать очередную строку.";
-			exit(0);
-		}
-	}
-
 }
 void giveMemory1(int *&B, const int N) {
 	try { B = new int[N]; }
@@ -29,23 +20,14 @@ void giveMemory1(int *&B, const int N) {
 		exit(0);
 	}
 }
-void initArray(int* *A, int N) { //Заполнение симметричной матрицы А(N, N), заданной верхним треугольником относительно побочной диагонали
-	for (int i = 0; i<N; i++) {
-		for (int j = 0; j < N; j++) {
-			if (j && (j - i)>=1) { A[i][j] = 0; }
-			else {
-				if (N % 2 && j&&i > N / 2 && (N - j - i <= 0)) {      
-					A[i][j] = A[N - j - 1][N - i - 1]; continue;
-				}
-				if (!(N % 2) && j&&i >= N / 2 && (N - j - i <= 0)) {    
-					A[i][j] = A[N - j - 1][N - i - 1]; continue;
-				}
-				cin >> A[i][j];
-			}
-		}
+
+
+
+void initArrayA(int *B, int N) {
+	for (int i = 0; i<N*(N+1)/2; i++) {
+		cin >> B[i];
 	}
 }
-
 
 
 
@@ -54,26 +36,19 @@ void initArrayB(int *B, int N) {
 		cin >> B[i];
 	}
 }
-void printArray(int **A, int N) {
-	for (int i = 0; i<N; i++) {
-		for (int j = 0; j<N; j++) {
-			cout << setw(3) << A[i][j];
-		}
-		cout << endl;
-	}
-}
+
 void printArrayB(int *B, int N) {
 	for (int i = 0; i<N; i++) {
 		cout << B[i];
 		cout << endl;
 	}
 }
-void freeMemory(int **A, const int N) {
-	for (int i = 0; i<N; i++) {
-		delete[]A[i];
-	}
+void freeMemory(int *A, const int N) {
+
+		delete[N]A;
+	
 }
-int help(int **A, int *B, int N) {
+int help(int *A, int *B, int N) {
 	cout << "Симметричную матрицу А(N,N), заданную нижним треугольником в виде одномерного массива относительно побочной диагонали, умножить на вектор В(N). В памяти матрицу не получать." << endl;
 	cout << "Пользователь вводит x:" << endl;
 	cout << "Программа симметрично относительно побочной оси отображает элементы y" << endl;
@@ -89,42 +64,48 @@ int help(int **A, int *B, int N) {
 	return 0;
 }
 
-int exit(int **A, int *B, int N) {
+int exit(int *A, int *B, int N) {
 	exit(0);
 }
-int input(int **A, int *B, int N) {
-	cout << "Введите элементы верхнетреугольной матрицы: " << "\n";
-	initArray(A, N);
-	cout << "Введите координаты вектра, на который будет умножаться матрица" << "\n";
-	initArrayB(B, N);
-	cout << "Матрица:" << endl;
-	printArray(A, N);
+
+
+
+int input(int *A, int *B, int N) {
+
+	cout << "Введите координаты матрицы, на который будет умножаться матрица" << "\n";
+	initArrayA(A, N);
 	cout << "Вектор" << endl;
-	printArrayB(B, N);
+	initArrayB(B, N);
+
 	return 0;
 }
-int output(int **A, int *B, int N) {
+int output(int *A, int *B, int N) {
 	int rez = 0;
-	cout << "Результат произведения: " << "\n";
-	for (int i = 0; i < N; i++) {
-		for (int j = 0, k = 0; j < N; j++, k++) {
-			rez += B[k] * A[i][j];
+	int digit = 0;
+	int k = N - 1;
+	for (int i = 0; i < N*(N+1)/2;) {
+		for (int j = 0; j <N-k; j++,i++) {
+			digit = A[i] * B[j];
+			rez = rez + digit;
+			
 		}
-		cout << rez << "\n";
+		cout << "Результат произведения:" << rez << endl;
+		k--;
 		rez = 0;
 	}
 	return 0;
 }
-void forEach(int **A, int* B, const int N, pfff pf) {
+void forEach(int *A, int* B, const int N, pfff pf) {
 	pf(A, B, N);
 }
 int main() {
 	setlocale(LC_ALL, "ru");
 	int N, n;
-	int* *A = nullptr;
-	int *B = nullptr;
-	cout << "Введите количество строк и столбцов в cимметричной матрице А(N, N): " <<endl;
+	cout << "Введите N";
 	cin >> N;
+	int *A=nullptr;
+	int *B=nullptr;
+	cout << "Введите количество строк и столбцов в cимметричной матрице А(N, N): " <<endl;
 	giveMemory(A, N);
 	giveMemory1(B, N);
 	
@@ -136,8 +117,8 @@ int main() {
 		cin >> n;
 		forEach(A, B, N, Fmenu[n]);
 	}
-	printArray(A, N);
 	freeMemory(A, N);
+	
 	system("pause");
 	return 0;
 }
